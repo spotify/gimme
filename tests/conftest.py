c3b9fbf4f8cc4ec03a329fe1ed554e0daa5d7f32
Daniele Sluijters <daenney@spotify.com>
2018-07-11 19:18:39 +0200

    tests: Fix freezing of time
    You cannot use the decorator form to freeze time during the creation of
    fixtures. Use context manager form instead.

--- tests/conftest.py
+++ tests/conftest.py
@@ -20,6 +20,7 @@ from __future__ import absolute_import, print_function, unicode_literals
 import time
 
 import pytest
+from freezegun import freeze_time
 
 from gimme.app import create_app
 from gimme.settings import Testing
@@ -33,56 +34,56 @@ def app():
 
 
 @pytest.fixture
-@pytest.mark.freeze_time('2018-05-04')
 def loggedin_app(app):
     """Creates a logged-in test client instance."""
-    with app.test_client() as client:
-        with client.session_transaction() as sess:
-            sess['domain'] = 'example.com'
-            sess['account'] = 'test@example.com'
-            sess['google_oauth_token'] = {
-                'access_token': 'tenarsotenarsoetinars',
-                'id_token': 'sotieanrsoietnarst',
-                'token_type': 'Bearer',
-                'expires_in': '3600',
-                'expires_at': time.time() + 3600,
-            }
-        yield client
+    with freeze_time('2018-05-04', tz_offset=0):
+        with app.test_client() as client:
+            with client.session_transaction() as sess:
+                sess['domain'] = 'example.com'
+                sess['account'] = 'test@example.com'
+                sess['google_oauth_token'] = {
+                    'access_token': 'tenarsotenarsoetinars',
+                    'id_token': 'sotieanrsoietnarst',
+                    'token_type': 'Bearer',
+                    'expires_in': '3600',
+                    'expires_at': time.time() + 3600,
+                }
+            yield client
 
 
 @pytest.fixture
-@pytest.mark.freeze_time('2018-05-04')
 def invalid_loggedin_app(app):
     """Creates a logged-in test client instance with an invalid domain."""
-    with app.test_client() as client:
-        with client.session_transaction() as sess:
-            sess['domain'] = 'example.org'
-            sess['account'] = 'test@example.org'
-            sess['google_oauth_token'] = {
-                'access_token': 'tenarsotenarsoetinars',
-                'id_token': 'sotieanrsoietnarst',
-                'token_type': 'Bearer',
-                'expires_in': '3600',
-                'expires_at': time.time() + 3600,
-            }
-        yield client
+    with freeze_time('2018-05-04', tz_offset=0):
+        with app.test_client() as client:
+            with client.session_transaction() as sess:
+                sess['domain'] = 'example.org'
+                sess['account'] = 'test@example.org'
+                sess['google_oauth_token'] = {
+                    'access_token': 'tenarsotenarsoetinars',
+                    'id_token': 'sotieanrsoietnarst',
+                    'token_type': 'Bearer',
+                    'expires_in': '3600',
+                    'expires_at': time.time() + 3600,
+                }
+            yield client
 
 
 @pytest.fixture
-@pytest.mark.freeze_time('2018-05-04')
 def incomplete_loggedin_app(app):
     """Creates a logged-in test client instance.
 
     This instances misses the keys in the session we require and will therefore
     trigger a lookup of the profile information
     """
-    with app.test_client() as client:
-        with client.session_transaction() as sess:
-            sess['google_oauth_token'] = {
-                'access_token': 'tenarsotenarsoetinars',
-                'id_token': 'sotieanrsoietnarst',
-                'token_type': 'Bearer',
-                'expires_in': '3600',
-                'expires_at': time.time() + 3600,
-            }
-        yield client
+    with freeze_time('2018-05-04', tz_offset=0):
+        with app.test_client() as client:
+            with client.session_transaction() as sess:
+                sess['google_oauth_token'] = {
+                    'access_token': 'tenarsotenarsoetinars',
+                    'id_token': 'sotieanrsoietnarst',
+                    'token_type': 'Bearer',
+                    'expires_in': '3600',
+                    'expires_at': time.time() + 3600,
+                }
+            yield client
